04fcb3a54e9b87d41666e5048de61030ef041c64
Abel Souza <abelsouzacosta@gmail.com>
2021-08-02 01:42:12 -0300

    Adiionando estrutura inicial da página

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <header className="topo">Topo</header>
-      <main className="principal">Principal</main>
-      <footer className="rodape">Rodapé</footer>
+      <header className="topo">
+        <h1 className="logo"></h1>
+        <ul>
+          <li>
+            <a href="">Navegação</a>
+          </li>
+        </ul>
+      </header>
+      <main className="principal">
+        <h2>Últimos Lançamentos</h2>
+        <div className="card">Card</div>
+      </main>
+      <footer className="rodape">
+        <p>Conteúdos cedidos pela editora Novatec &#8212; Copyright 2021</p>
+      </footer>
     </>
   );
 };
